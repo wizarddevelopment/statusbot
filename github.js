1706070a4af5d970378802ba4e836ddbfb8b2853
Francis Gulotta <wizard@roborooter.com>
2014-07-04 21:01:49 -0400

    Ignore github for a while, tell circle to report to us, and test success

--- github.js
+++ github.js
@@ -1,7 +1,7 @@
 var util = require('util');
 
 var githubNotifier = function (req, res, next) {
-  console.log('github', req.url, req.body, req.method);
+  // console.log('github', req.url, req.body, req.method);
   res.end();
 };
 
